ce590ca49fb57a5784b1db2722a9bb21f355f02b
Anonymous20240125 <157742453+AnonymousLYZYY@users.noreply.github.com>
2024-01-28 02:36:38 +0800

    Update no_multi.py

--- WP/no_multi.py
+++ WP/no_multi.py
@@ -131,14 +131,14 @@ def no_multi(story_content, messages):
 
 def main():
     try:
-        with open('/media3/shiyi/agent/multi-agent-new/WP/debate_result_new/no_multi.json', 'r',
+        with open('no_multi.json', 'r',
                   encoding='utf-8') as file:
             no_multi_data = json.load(file)
     except FileNotFoundError:
         no_multi_data = []
 
     try:
-        with open('/media3/shiyi/agent/multi-agent-new/WP/debate_result_new/no_multi.pkl',
+        with open('no_multi.pkl',
                   'rb') as progress_file:
             start_index = pickle.load(progress_file)
     except FileNotFoundError:
